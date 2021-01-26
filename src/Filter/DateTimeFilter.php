b404dba08edbbe7039d3017cf2bf171584af5283
Daniel Opitz <d.opitz@outlook.com>
2021-01-26 15:35:41 +0100

    Init

--- src/Filter/DateTimeFilter.php
+++ src/Filter/DateTimeFilter.php
@@ -4,6 +4,8 @@ namespace Selective\Transformer\Filter;
 
 use DateTimeImmutable;
 use DateTimeZone;
+use Exception;
+use Selective\Transformer\Exceptions\ArrayTransformerException;
 
 /**
  * Filter.
@@ -17,21 +19,30 @@ final class DateTimeFilter
      * @param string|null $format
      * @param DateTimeZone|null $timezone
      *
+     * @throws Exception
+     *
      * @return mixed The value
      */
     public function __invoke($value, string $format = null, DateTimeZone $timezone = null)
     {
-        $format = $format ?? 'Y-m-d H:i:s';
+        try {
+            $format = $format ?? 'Y-m-d H:i:s';
 
-        if (is_string($value)) {
-            return (string)(new DateTimeImmutable($value, $timezone))->format($format);
+            if (is_string($value)) {
+                return (string)(new DateTimeImmutable($value, $timezone))->format($format);
+            }
+
+            if ($value instanceof DateTimeImmutable) {
+                if ($timezone) {
+                    $value->setTimezone($timezone);
+                }
+
+                return (string)$value->format($format);
+            }
+
+            return null;
+        } catch (Exception $exception) {
+            throw new ArrayTransformerException($exception->getMessage(), $exception->getCode(), $exception);
         }
-
-        if ($value instanceof DateTimeImmutable) {
-            return (string)$value->format($format);
-        }
-
-        // Invalid value
-        return null;
     }
 }
